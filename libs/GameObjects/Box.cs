8c650ebb4436b100f278f369fef99c8af724b830
car0linee <caroline.mandl@outlook.com>
2024-06-30 20:21:10 +0200

    Fixed Star/Note Error
    accidentally deleted those lines in earlier commit

--- libs/GameObjects/Box.cs
+++ libs/GameObjects/Box.cs
@@ -2,10 +2,24 @@ namespace libs;
 
 public class Box : GameObject
 {
+    private GameObjectFactory gameObjectFactory;
+
+    public Map map = GameEngine.Instance.GetMap();
     public Box() : base()
     {
+        this.gameObjectFactory = (GameEngine.Instance.gameObjectFactory as GameObjectFactory);
         Type = GameObjectType.Player;
-        CharRepresentation = '★';
+        CharRepresentation = '○';
+
+        if (GameEngine.Instance.currentLevel == 1)
+        {
+            CharRepresentation = '★';
+        }
+        else if (GameEngine.Instance.currentLevel == 2)
+        {
+            CharRepresentation = '♫';
+        }
+
         Color = ConsoleColor.DarkGreen;
     }
 
